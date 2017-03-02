3eacedec7d9ae6f5da04e2b8f2dd7c021e9fca1f
Evan Scott <evansco@umich.edu>
2017-03-02 01:05:39 -0500

    Improved flexibility for dependencies of test files

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-## gcovmagic
+## Synopsis
 
 gcovmagic is a tool to simplify unit testing with gcov
+
+## Arguments
+
+usage: gcovmagic.py [-h] FileToTest [-t TESTS [TESTS ...]] [-d [TESTDEPS [TESTDEPS ...]]]
+
+## Motivation
+
+gcov is a fantastic tool to get coverage statistics when unit testing a project, but it can certainly be a bit of a beast. This script simplifies the process of unit testing by compiling and linking the source files so that the gcov notes files are not overwritten by each successive compilation of a unit test.
+
+## Installation
+
+Make sure you have gcov installed.
+The script should work out of the box.
--- gcovmagic.py
+++ gcovmagic.py
@@ -9,21 +9,31 @@ def main():
 	
 	parser = argparse.ArgumentParser()
 	parser.add_argument('FileToTest', help='File to test coverage of')
-	parser.add_argument('Tests', help='Test files to compile', nargs='+')
+	parser.add_argument('-t', help='Test files to compile', nargs='+', dest='tests')
+	parser.add_argument('-d', help='Dependencies to link with test files', nargs='*', dest='testDeps')
+	
 	args = parser.parse_args()
 	
 	FileToTest = args.FileToTest[:-4]
+	tests = args.tests
+	testDeps = args.testDeps
 	
-	subprocess.call(['g++', '-std=c++11', '-g', '-O0', '--coverage', args.FileToTest, '-c', '-o', FileToTest+'.o'])
+	testDepObjs = []
 	
-	for test in args.Tests:
+	for testDep in testDeps:
+		subprocess.call(['g++', '-std=c++11', '-g', '-O0', '--coverage', testDep, '-c', '-o', testDep[:-4]+'.o'])
+		testDepObjs.append(testDep[:-4]+'.o')
+	
+	for test in tests:
 		subprocess.call(['g++', '-std=c++11', '-g', '-O0', '--coverage', test, '-c', '-o', test[:-4]+'_cov.o'])
-		subprocess.call(['g++', '-std=c++11', '-g', '--coverage', FileToTest+'.o', test[:-4]+'_cov.o', '-o', test[:-4]+'_cov'])
+		subprocess.call(['g++', '-std=c++11', '-g', '--coverage'] + testDepObjs + [test[:-4]+'_cov.o', '-o', test[:-4]+'_cov'])
 	
-	for test in args.Tests:
+	for test in tests:
 		subprocess.call(['./' + test[:-4]+'_cov'])
 		
-	subprocess.call(['gcov', args.FileToTest])
+	gcovOutput = subprocess.check_output(['gcov', args.FileToTest]).split('\n')
+	print(gcovOutput[0])
+	print(gcovOutput[1])
 	subprocess.call(['mv', args.FileToTest+'.gcov', 'coverage.txt'])
 	
 	clean()
